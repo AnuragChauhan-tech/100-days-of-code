125d35fb091d4625c0f7298e9539c6aa51914f22
Anurag Chauhan <nanuchauhan2006@gmail.com>
2025-10-01 12:10:19 +0530

    Add files via upload

---
+++ Q25.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    char operator;
+
+    printf("Enter two numbers and an operator (+, -, *, /,  % ): ");
+    scanf("%d %d %c", &num1, &num2, &operator);
+
+    switch (operator) {
+        case '+':
+            printf("%d\n", num1 + num2);
+            break;
+        case '-':
+            printf("%d\n", num1 - num2);
+            break;
+        case '*':
+            printf("%d\n", num1 * num2);
+            break;
+        case '/':
+            if (num2 != 0) {
+                printf("%d\n", num1 / num2);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        case '%':
+            if (num2 != 0) {
+                printf("%d\n", num1 % num2);
+            } else {
+                printf("Error: Division by zero\n");
+            }
+            break;
+        default:
+            printf("Invalid operator\n");
+    }
+
+    return 0;
+}
