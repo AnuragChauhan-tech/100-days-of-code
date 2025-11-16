18d6d05bcacdc71dc2b7bd5c90b9feb82c2ea732
Anurag Chauhan <nanuchauhan2006@gmail.com>
2025-11-16 23:21:58 +0530

    Add files via upload

---
+++ Q 75.c
@@ -0,0 +1,43 @@
+//Add  the two matrices
+#include <stdio.h>
+int main(){
+    int rows,cols;
+    printf("Enter the number of rows and columns: ");
+    scanf("%d %d",&rows,&cols);
+    int mat1[rows][cols] ,mat2[rows][cols];
+    int sumMat[rows][cols];
+
+    printf("Enter elements of array 1: ");
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<cols;j++){
+            scanf("%d", &mat1[i][j]);
+        }
+    }
+
+    printf("Enter elements of array 2: ");
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<cols;j++){
+            scanf("%d", &mat2[i][j]);
+        }
+    }
+
+    for (int row=0;row<rows;row++){
+        
+        for (int col=0;col<cols;col++){
+            sumMat[row][col] = mat1[row][col] + mat2[row][col];
+        }
+    }
+
+    
+
+    for (int i=0;i<rows;i++){
+        for(int j=0;j<cols;j++){
+            printf("%d ",sumMat[i][j]);
+
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
